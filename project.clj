04441e3c3d865c7dcf8ab1bc9113db1d358e4964
Min Huang <min.huang@alumni.usc.edu>
2010-11-11 17:18:21 -0800

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,10 @@
 (defproject robust-txt "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+  :description "A robots.txt parser."
+  :repositories
+    {"clojars" "http://clojars.org/repo"
+     "lousycoder" "http://maven.lousycoder.com"}
+  :dependencies
+    [[org.clojure/clojure "1.2.0"]
+     [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies
+    [[lein-run "1..0.0"]])
