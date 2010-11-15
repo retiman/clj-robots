722c1159d9d0161536900251e337298eed78d545
Min Huang <min.huang@alumni.usc.edu>
2010-11-15 01:30:20 -0800

    Some preliminary work in supporting multiple sections

--- src/robust_txt/core.clj
+++ src/robust_txt/core.clj
@@ -6,16 +6,51 @@
   (:gen-class))
 
 (defn trim-comment
-  "Strip out comments from a line."
   [line]
   (su/replace line #"#.*$" ""))
 
-(defn parse-line [line]
-  (let [[left right] (su/split (trim-comment line) #":" 2)]
-    (if (nil? right)
-      nil
-      [(su/lower-case (su/trim left)) (su/trim right)])))
+(defn process-user-agent
+  [result user-agent value]
+  (dosync
+    (ref-set user-agent value)
+    (alter result assoc @user-agent [])))
 
-;(defn parse [lines]
-;  (let [state (ref :start)
-;        result (ref {})
+(defn process-allow
+  [result user-agent value]
+  (dosync
+    (let [permissions (@result @user-agent)]
+      (alter result
+             assoc @user-agent (vec (conj permissions [:allow value]))))))
+
+(defn process-disallow
+  [result user-agent value]
+  (dosync
+    (let [permissions (@result @user-agent)]
+      (alter result
+             assoc @user-agent (vec (conj permissions [:disallow value]))))))
+
+(defn process-directive
+  [result key value]
+  (dosync
+    (alter result assoc key value)))
+
+(defn parse-line
+  [line]
+  (let [[key value] (su/split (trim-comment line) #":" 2)]
+    (if (nil? value)
+      nil
+      [(su/lower-case (su/trim key)) (su/trim value)])))
+
+(defn parse-lines
+  [lines]
+  (let [user-agent (ref "*")
+        result (ref {})]
+    (do
+      (doseq [line lines]
+        (let [[key value] (parse-line line)]
+          (cond
+            (= key "user-agent") (process-user-agent result user-agent value)
+            (= key "allow")      (process-allow result user-agent value)
+            (= key "disallow")   (process-disallow result user-agent value)
+            :default             (process-directive result key value))))
+      result)))
--- test/robust_txt/test/core.clj
+++ test/robust_txt/test/core.clj
@@ -1,8 +1,10 @@
 (ns robust-txt.test.core
   (:use
     [robust-txt.core]
-    [robust-txt.utils :only (load-resource)]
-    [clojure.test]))
+    [robust-txt.utils :only (load-resource stream-to-string)]
+    [clojure.test])
+  (:require
+    [clojure.contrib.str-utils2 :as su]))
 
 (deftest test-parse-line
   (do
@@ -10,3 +12,10 @@
            (parse-line "UsEr-AgEnt: :*:# This is a comment")))
     (is (nil? (parse-line "user-agent*")))
     (is (nil? (parse-line "")))))
+
+(deftest test-parse-lines
+  (let [stream (load-resource "robust_txt/test/robots.txt")
+        string (stream-to-string stream)
+        lines (su/split-lines string)
+        result (parse-lines lines)]
+    (println result)))
