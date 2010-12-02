939af9d57f5fabebb073612db828a9df8e6edd89
Min Huang <min.huang@alumni.usc.edu>
2010-12-02 03:02:07 -0800

    Utilities for testing private functions

---
+++ test/robust_txt/test/utils.clj
@@ -0,0 +1,9 @@
+(ns robust-txt.test.utils
+  (:gen-class))
+
+(defn refer-private
+  "Access private symbols of a namespace."
+  [ns]
+  (doseq [[symbol var] (ns-interns ns)]
+    (when (:private (meta var))
+      (intern *ns* symbol var))))
