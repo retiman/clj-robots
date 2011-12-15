6b1dda1068dda7bc4df5b78d84ebf65930257b0f
Min Huang <min.huang@alumni.usc.edu>
2011-12-14 18:46:31 -0800

    Update for Clojure 1.3.0

--- project.clj
+++ project.clj
@@ -1,19 +1,17 @@
-(defproject clj-robots "0.5.1-2"
+(defproject clj-robots "0.6.0"
   :description "A robots.txt parser."
-  :min-lein-version "1.4.2"
+  :min-lein-version "1.6.2"
   :warn-on-reflection true
   :repositories
     {"clojars" "http://clojars.org/repo"}
   :dependencies
-    [[org.clojure/clojure "1.2.0"]
-     [org.clojure/clojure-contrib "1.2.0"]
-     [clj-time "0.3.0"]
-     [commons-io "2.0"]
-     [clj-httpc "1.7.1-2"]]
+    [[org.clojure/clojure "1.3.0"]
+     [clj-time "0.3.3"]
+     [commons-io "2.0"]]
   :dev-dependencies
-    [[autodoc "0.7.1"]
-     [lein-clojars "0.6.0"]
-     [robert/hooke "1.1.0"]]
+    [[backtype/autodoc "0.9.0-SNAPSHOT"]
+     [lein-clojars "0.7.0"]
+     [robert/hooke "1.1.2"]]
   :test-selectors
     {:default (fn [t] (not (:integration t)))
      :integration :integration
